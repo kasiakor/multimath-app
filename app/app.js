f6e0206312a7c2ae5358b57e5c9eb25db88cbfd0
kasia.kordys <kasia.kordys@gmail.com>
2021-11-09 17:08:42 +0200

    add compiler options

--- app/app.js
+++
@@ -1,5 +0,0 @@
-function startGame() {
-    var messagesElement = document.getElementById("messages");
-    messagesElement.innerText = "Welcome to MultiMath, Let's start a new game!";
-}
-document.getElementById("startGame").addEventListener("click", startGame);
