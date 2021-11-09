f650d80cff650d98f9310455a0f202c95368d542
kasia.kordys <kasia.kordys@gmail.com>
2021-11-09 16:09:20 +0200

    add gitignore file and basic html layout

--- app/app.js
+++ app/app.js
@@ -2,5 +2,4 @@ function startGame() {
     var messagesElement = document.getElementById("messages");
     messagesElement.innerText = "Welcome to MultiMath, Let's start a new game!";
 }
-
 document.getElementById("startGame").addEventListener("click", startGame);
