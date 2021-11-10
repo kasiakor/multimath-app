b058024c6f6f2c5020b240c8f584e92b06e6a557
kasia.kordys <kasia.kordys@gmail.com>
2021-11-10 18:30:38 +0200

    allow strict null checks

---
+++ app/app.js
@@ -0,0 +1,10 @@
+function startGame() {
+    var playersName = "Gia";
+    logPlayer(playersName);
+    var messagesElement = document.getElementById("messages");
+    messagesElement.innerText = "Welcome to MultiMath, Let's start a new game!";
+}
+function logPlayer(name) {
+    console.log("The new game starts for player " + name);
+}
+document.getElementById("startGame").addEventListener("click", startGame);
--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,15 @@
 function startGame() {
     //start new game
+
+    let playersName: string | undefined;
+    playersName =  undefined;
+    logPlayer(playersName);
     var messagesElement = document.getElementById("messages");
     messagesElement!.innerText = "Welcome to MultiMath, Let's start a new game!";
+}
+
+function logPlayer(name: string | undefined) {
+    console.log(`The new game starts for player ${name}`);
 }
 
 document.getElementById("startGame")!.addEventListener("click", startGame);
