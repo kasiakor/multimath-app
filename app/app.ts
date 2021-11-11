60d3723c98583180bdaa5deae193f654ce7d50b0
kasia.kordys <kasia.kordys@gmail.com>
2021-11-11 15:21:54 +0200

    add function to get input value and display it to the user

--- app/app.ts
+++ app/app.ts
@@ -1,15 +1,30 @@
 function startGame() {
     //start new game
 
-    let playersName: string | undefined;
-    playersName =  undefined;
+    let playersName: string | undefined =  getInputValue("playername");
     logPlayer(playersName);
-    var messagesElement = document.getElementById("messages");
-    messagesElement!.innerText = "Welcome to MultiMath, Let's start a new game!";
+
+    postScore(100, playersName);
 }
 
-function logPlayer(name: string | undefined) {
+function logPlayer(name: string = "MultiMath Player"): void {
     console.log(`The new game starts for player ${name}`);
+}
+
+function getInputValue(elementID: string): string | undefined {
+    const elementTarget: HTMLInputElement = <HTMLInputElement>document.getElementById(elementID);
+
+    if(elementTarget.value === "") {
+        return undefined;
+    }
+    else {
+        return elementTarget.value;
+    }
+}
+
+function postScore(score: number, playersName: string = "MultiMath Player"): void {
+    const scoreTarget: HTMLElement | null  = <HTMLElement>document.getElementById("postedScores");
+    scoreTarget.innerText = `${score} - ${playersName}`;
 }
 
 document.getElementById("startGame")!.addEventListener("click", startGame);
